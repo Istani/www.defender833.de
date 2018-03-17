131dc93283638cd1382bca4df7ce865d14f23e07
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-03-18 00:35:20 +0100

    About Page

--- index.php
+++ index.php
@@ -1,7 +1,88 @@
-<?php 
-    echo 'Hello World!';
-    echo '<pre>';
-    echo var_dump($_GET);
-    echo '</pre>';
-    echo '<hr>';
+<?php
+require_once "config.php";
 ?>
+<!doctype html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  
+  <title><?php echo $SYTH['youtube_channel']['youtube_snippet_title']; ?></title>
+  <link href="<?php echo $site['base_dir']; ?>style/main_style.css" type="text/css" rel="stylesheet">
+  
+  <script type="text/javascript">
+  function change_all_links(){
+    for (var linkNum = 0; linkNum < document.links.length; linkNum++){
+      var findTxt = 'http://<?php echo $_SERVER["HTTP_HOST"]; ?>';
+      var linktext = String(document.links[linkNum]);
+      var sublink=linktext.substr(0,findTxt.length);
+      if (sublink!=findTxt) {
+        document.links[linkNum].target="_blank";
+      }
+    }
+  };
+</script>
+
+</head>
+<body>
+  <center>
+    <table>
+      <tr>
+        <td>
+          <div class="div-border rounded-corner div-shadow" style="width:1060px;height:173px">
+            <?php
+            echo '<img src="'.$SYTH['youtube_channel']['youtube_brandingsettings_image_bannerimageurl'].'" alt="'.$SYTH['youtube_channel']['youtube_snippet_title'].' banner">';
+            ?>
+          </div>
+          <br>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <div class="div-border rounded-corner div-shadow">
+            <ul>
+              <li><a href="<?php echo $site['base_dir']; ?>">About me</a></li>
+              <li><a href="<?php echo $site['base_dir']; ?>/Videos">Videos</a></li>
+              <li><a href="<?php echo $site['base_dir']; ?>/Games">Games & More</a></li>
+              
+            </ul>
+          </div>
+          <br>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <?php
+          if (!isset($_GET['kategorie'])) {$_GET['kategorie']="";}
+          if (!isset($_GET['details'])) {$_GET['details']="";}
+          
+          switch ($_GET['kategorie']) {
+            
+            default:
+            include("pages/about_me.php");
+            
+          }
+          
+          ?>
+          <br>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <div class="div-border rounded-corner div-shadow">
+            FOOTER
+          </div>
+          <br>
+        </td>
+      </tr>
+    </table>
+  </center>
+  <?php
+  echo '<pre>';
+  echo var_dump($_SERVER);
+  echo '</pre>';
+  ?>
+  <script type="text/javascript">
+  change_all_links();
+  </script>
+</body>
+</html>
---
+++ pages/about_me.php
@@ -0,0 +1,63 @@
+<div class="div-border rounded-corner div-shadow div-content">
+  <h1>About: <?php echo $SYTH['youtube_channel']['youtube_snippet_title']; ?></h1>
+  
+  <table width="100%">
+    <tr>
+      <td><img src="<?php echo $SYTH['youtube_channel']['youtube_snippet_thumbnails_default_url']; ?>" alt="<?php echo $SYTH['youtube_channel']['youtube_snippet_title']; ?> avatar"></td>
+      <td>
+        <table>
+          <tr>
+            <td> &nbsp; </td><td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td> Gründung: </td><td><?php echo date("d.m.Y",strtotime($SYTH['youtube_channel']['youtube_snippet_publishedat'])); ?></td>
+          </tr>
+          <tr>
+            <td> Videos: </td><td><?php echo $SYTH['youtube_channel']['youtube_statistics_videocount']; ?></td>
+          </tr>
+          <tr>
+            <td> Abonennten: </td><td><?php echo $SYTH['youtube_channel']['youtube_statistics_subscribercount']; ?></td>
+          </tr>
+          <tr>
+            <td> Kanal: </td><td><a href="https://www.youtube.com/<?php echo $SYTH['youtube_channel']['youtube_snippet_customurl']; ?>"><?php echo "https://www.youtube.com/".$SYTH['youtube_channel']['youtube_snippet_customurl']; ?></a></td>
+          </tr>
+        </table>
+      </td>
+      <td>
+        <table>
+          <tr>
+            <td> &nbsp; </td><td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td> &nbsp; </td><td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td> Views: </td><td><?php echo $SYTH['youtube_channel']['youtube_statistics_viewcount']; ?></td>
+          </tr>
+          <tr>
+            <td> &nbsp; </td><td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td> Livestream: </td><td><a href="http://gaming.youtube.com/channel/<?php echo $SYTH['youtube_channel']['youtube_id']; ?>/live">Youtube Gaming</a></td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
+  <hr>
+  <br>
+  
+  <center>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/<?php echo $SYTH['youtube_channel']['youtube_brandingsettings_channel_unsubscribedtrailer']; ?>" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+  </center>
+  
+  <br>
+  <hr>
+  <br>
+  <?php
+  
+  
+  echo nl2br($SYTH['youtube_channel']['youtube_snippet_description']);
+  ?>
+  <br><br>
+</div>
