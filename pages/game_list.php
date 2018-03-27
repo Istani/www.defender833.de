e6b2476e261b7afaaa130ab921d4c74f6e02d5a8
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-03-27 13:25:21 +0200

    Gamelist_Filter

--- pages/game_list.php
+++ pages/game_list.php
@@ -2,12 +2,12 @@
       <h1>Games & More:</h1>
 <?php
 $price_collums="steam_price,humble_price";
-$game_price=$database->sql_select("bot_gamelist","*, 
+$game_price=$database->sql_select("bot_gamelist LEFT JOIN bot_gamelist_video ON bot_gamelist.name=bot_gamelist_video.name","bot_gamelist.*, bot_gamelist_video.count_video
 GREATEST(".$price_collums.") AS max_price,
 LEAST(".$price_collums.") AS min_price,
 (GREATEST(".$price_collums.")-LEAST(".$price_collums.")) AS diff_price
-","description NOT LIKE '' AND steam_price>0 
-ORDER BY diff_price DESC",true);
+","description NOT LIKE '' AND steam_price>0 AND (bot_gamelist_video.channel IS NULL OR bot_gamelist_video.channel='".$SYTH['user']['youtube_user']."'
+ORDER BY bot_gamelist_video.count_video DESC, diff_price DESC",true);
 
 echo '<pre>';
 var_dump($game_price);
