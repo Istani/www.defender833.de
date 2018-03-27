a62cc0aeec122f11fb94d73849a5a2c7749b29b4
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-03-27 14:17:57 +0200

    Video List DIV

--- pages/game_list.php
+++ pages/game_list.php
@@ -9,8 +9,21 @@ LEAST(".$price_collums.") AS min_price,
 ","description NOT LIKE '' AND steam_price>0 AND (bot_gamelist_video.channel IS NULL OR bot_gamelist_video.channel='".$SYTH['user']['youtube_user']."')
 ORDER BY CAST(bot_gamelist_video.count_video AS UNSIGNED) DESC, diff_price DESC",true);
 
+for ($count_games=0;$count_games<count($game_price);$count_games++) {
+      $tg=$game_price[$count_games];
+?>
+      <div class="div-border rounded-corner div-shadow div-content">
+            <img src="<?php echo $tg['banner']; ?>" alt="<?php echo $tg['name']; ?>"><br>
+            <?php echo $tg['name']; ?><br>
+            <?php echo $tg['min_price']; ?>€ - <?php echo $tg['max_price']; ?>€<br>
+            <?php echo $tg['count_video']; ?> Videos<br>
+      </div>
+<?php
+}
+/*
 echo '<pre>';
 var_dump($game_price);
 echo '</pre>';
+*/
 ?>
 </div>
