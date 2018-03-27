ff041da568020e8ede6382768db1eead86930e5c
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-03-27 12:15:16 +0200

    Die Richtigen Games

--- pages/game_list.php
+++ pages/game_list.php
@@ -4,8 +4,10 @@
 
 $game_price=$database->sql_select("bot_gamelist","*, 
 GREATEST(steam_price,humble_price) AS max_price,
-LEAST(steam_price,humble_price) AS min_price
-","true",true);
+LEAST(steam_price,humble_price) AS min_price,
+(max_price-min_price) AS diff_price
+","description NOT LIKE '' 
+ORDER BY diff_price DESC",true);
 
 echo '<pre>';
 var_dump($game_price);
