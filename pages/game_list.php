099c59a4208e064651b027601135acfd8f03188a
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-03-27 12:41:16 +0200

    Spiele welche keinen Richtigen Steam Preis haben ausblenden

--- pages/game_list.php
+++ pages/game_list.php
@@ -6,7 +6,7 @@ $game_price=$database->sql_select("bot_gamelist","*,
 GREATEST(".$price_collums.") AS max_price,
 LEAST(".$price_collums.") AS min_price,
 (GREATEST(".$price_collums.")-LEAST(".$price_collums.")) AS diff_price
-","description NOT LIKE '' 
+","description NOT LIKE '' AND steam_price>0 
 ORDER BY diff_price DESC",true);
 
 echo '<pre>';
