b6383490cd2fb3764d26224a537bae3287d99d8c
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-04-23 20:52:11 +0200

    div

--- pages/impressum.php
+++ pages/impressum.php
@@ -1,8 +1,11 @@
-<?php
-
-?>
-Defender833 - Martin Koytek<br>
-Sandkaulstr. 70<br>
-52062 Aachen<br>
-
-Mail: <a href="mailto:martin@defender833.de">martin@defender833.de</a><br>
+<div class="div-border rounded-corner div-shadow div-content">
+  <h1>Impressum:</h1>
+  &nbsp;<br>
+  
+  Defender833 - Martin Koytek<br>
+  Sandkaulstr. 70<br>
+  52062 Aachen<br>
+  
+  Mail: <a href="mailto:martin@defender833.de">martin@defender833.de</a><br>
+  <br><br>
+</div>
