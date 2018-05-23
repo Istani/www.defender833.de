b4ae540c8a15e3833416d6832924b0af86fce50e
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-05-23 15:54:23 +0200

    Game List Fix?

--- pages/game_list.php
+++ pages/game_list.php
@@ -1,39 +1,39 @@
 <div class="div-border rounded-corner div-shadow div-content">
-      <h1>Games & More:</h1>
-      &nbsp;<br>
-      <center>
-            <div>
-<?php
-$game_price=$database->sql_select("bot_gamelist LEFT JOIN bot_gamelist_video ON bot_gamelist.name=bot_gamelist_video.name","bot_gamelist.*, bot_gamelist_video.count_video,
-GREATEST(".$price_collums.") AS max_price,
-LEAST(".$price_collums.") AS min_price,
-(GREATEST(".$price_collums.")-LEAST(".$price_collums.")) AS diff_price
-","description NOT LIKE '' AND LEAST(".$price_collums.")>0 AND (bot_gamelist_video.channel IS NULL OR bot_gamelist_video.channel='".$SYTH['user']['youtube_user']."')
-ORDER BY CAST(bot_gamelist_video.count_video AS UNSIGNED) DESC, diff_price DESC, bot_gamelist.name",true);
-
-for ($count_games=0;$count_games<count($game_price);$count_games++) {
-      $tg=$game_price[$count_games];
-      // 460 - 215
-      // 1060 / 5 - (5*20)
-      // 192 - 89
-?>
-                  <a href="<?php echo $site['base_dir']; ?>Game/<?php echo str_replace(" ","_", $tg['name']); ?>" class="link">
-                        <div class="div-border rounded-corner div-shadow div-game">
-                              <img src="<?php echo $tg['banner']; ?>" alt="<?php echo $tg['name']; ?>" width="192" height="89"><br>
-                              <?php echo $tg['name']; ?><br>
-                              <?php echo $tg['min_price']; ?>€ - <?php echo $tg['max_price']; ?>€<br>
-                              &nbsp;<br>
-                        </div>
-                  </a>
-<?php
-}
-/*
-echo '<pre>';
-var_dump($game_price);
-echo '</pre>';
-*/
-?>
-                  <span class="break-game"></span>
-            </div>     
-      </center>
+  <h1>Games & More:</h1>
+  &nbsp;<br>
+  <center>
+    <div>
+      <?php
+      $game_price=$database->sql_select("bot_gamelist LEFT JOIN bot_gamelist_video ON bot_gamelist.name=bot_gamelist_video.name","bot_gamelist.*, bot_gamelist_video.count_video,
+      GREATEST(".$price_collums.") AS max_price,
+      LEAST(".$price_collums.") AS min_price,
+      (GREATEST(".$price_collums.")-LEAST(".$price_collums.")) AS diff_price
+      ","description NOT LIKE '' AND LEAST(".$price_collums.")>0 AND (bot_gamelist_video.channel IS NULL OR bot_gamelist_video.channel='".$SYTH['user']['youtube_user']."')
+      ORDER BY CAST(bot_gamelist_video.count_video AS UNSIGNED) DESC, diff_price DESC, bot_gamelist.name",false);
+      
+      for ($count_games=0;$count_games<count($game_price);$count_games++) {
+        $tg=$game_price[$count_games];
+        // 460 - 215
+        // 1060 / 5 - (5*20)
+        // 192 - 89
+        ?>
+        <a href="<?php echo $site['base_dir']; ?>Game/<?php echo str_replace(" ","_", $tg['name']); ?>" class="link">
+          <div class="div-border rounded-corner div-shadow div-game">
+            <img src="<?php echo $tg['banner']; ?>" alt="<?php echo $tg['name']; ?>" width="192" height="89"><br>
+            <?php echo $tg['name']; ?><br>
+            <?php echo $tg['min_price']; ?>€ - <?php echo $tg['max_price']; ?>€<br>
+            &nbsp;<br>
+          </div>
+        </a>
+        <?php
+      }
+      /*
+      echo '<pre>';
+      var_dump($game_price);
+      echo '</pre>';
+      */
+      ?>
+      <span class="break-game"></span>
+    </div>
+  </center>
 </div>
